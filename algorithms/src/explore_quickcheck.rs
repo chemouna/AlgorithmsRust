afeee7cd2f73f7635212d4b0f1b4a8154da5bd7c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-08-16 17:51:08 +0100

    Making testing work in rust.

--- algorithms/src/explore_quickcheck.rs
+++ algorithms/src/explore_quickcheck.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+//#[macro_use]
+
 extern crate quickcheck;
 
 use quickcheck::{TestResult, quickcheck};
@@ -14,12 +17,28 @@ fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
     xs == reverse(&reverse(&xs))
 }*/
 
-fn main() {
-    fn prop(xs: Vec<isize>) -> TestResult {
-        if xs.len() != 1 {
-            return TestResult::discard()
+#[cfg(test)]
+mod tests {
+    /*quickcheck! {
+        fn prop(xs: Vec<isize>) -> TestResult {
+            if xs.len() != 1 {
+                return TestResult::discard()
+            }
+            TestResult::from_bool(xs == reverse(&*xs))
         }
-        TestResult::from_bool(xs == reverse(&*xs))
+    }*/
+
+    fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
+        (
+            (b.0 - a.0).powi(2) +
+                (b.1 - a.1).powi(2)
+        ).sqrt()
     }
-    quickcheck(prop as fn(Vec<isize>) -> TestResult);
+
+    #[test]
+    fn distance_test() {
+        assert!(distance((0f32, 0f32), (1f32, 1f32)) == (2f32).sqrt());
+    }
+
+
 }
--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+#[macro_use]
+
+extern crate quickcheck;
+
+use quickcheck::{TestResult, quickcheck};
+
+fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+    let mut rev = vec!();
+    for x in xs.iter() {
+        rev.insert(0, x.clone())
+    }
+    rev
+}
+
+/*fn prop_reverse_idempotent(xs: Vec<u32>) -> bool {
+    xs == reverse(&reverse(&xs))
+}*/
+
+#[cfg(test)]
+mod tests {
+    /*quickcheck! {
+        fn prop(xs: Vec<isize>) -> TestResult {
+            if xs.len() != 1 {
+                return TestResult::discard()
+            }
+            TestResult::from_bool(xs == reverse(&*xs))
+        }
+    }*/
+
+    fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
+        (
+            (b.0 - a.0).powi(2) +
+                (b.1 - a.1).powi(2)
+        ).sqrt()
+    }
+
+    #[test]
+    fn distance_test() {
+        assert!(distance((0f32, 0f32), (1f32, 1f32)) == (2f32).sqrt());
+    }
+
+
 }
