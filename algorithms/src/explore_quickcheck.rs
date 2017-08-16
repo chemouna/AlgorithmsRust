1c39c5264bbf02d31c39b9b3ce642af3d1fd7c84
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-08-16 18:40:54 +0100

    Continue adding quickcheck.

--- algorithms/src/explore_quickcheck.rs
+++ algorithms/src/explore_quickcheck.rs
@@ -3,42 +3,25 @@
 
 extern crate quickcheck;
 
-use quickcheck::quickcheck;
-
-fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
-    let mut rev = vec!();
-    for x in xs.iter() {
-        rev.insert(0, x.clone())
-    }
-    rev
-}
-
-/*fn prop_reverse_idempotent(xs: Vec<u32>) -> bool {
-    xs == reverse(&reverse(&xs))
-}*/
-
 #[cfg(test)]
 mod tests {
+    use quickcheck::TestResult;
+
+    fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+        let mut rev = vec!();
+        for x in xs.iter() {
+            rev.insert(0, x.clone())
+        }
+        rev
+    }
+
     quickcheck! {
-        fn prop(xs: Vec<isize>) -> TestResult {
+        fn prop_one_element_vector_same(xs: Vec<isize>) -> TestResult {
             if xs.len() != 1 {
                 return TestResult::discard()
             }
             TestResult::from_bool(xs == reverse(&*xs))
         }
     }
-
-    fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
-        (
-            (b.0 - a.0).powi(2) +
-                (b.1 - a.1).powi(2)
-        ).sqrt()
-    }
-
-    #[test]
-    fn distance_test() {
-        assert!(distance((0f32, 0f32), (1f32, 1f32)) == (2f32).sqrt());
-    }
-
 
 }
